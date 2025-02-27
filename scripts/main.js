61a23d43bc38877418236cde20371880e260de5a
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-02-27 13:29:37 -0800

    Update main.js
    One successful step of progressive checkboxes : cb2 to cb1.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
+const cb1 = document.getElementById('cb1');
+const cb2 = document.getElementById('cb2');
+const cb3 = document.getElementById('cb3');
+const cb4 = document.getElementById('cb4');
 
+cb2.addEventListener('click', function() {
+	if (cb1.checked == true) {
+		cb1.checked = false;
+    } else {cb1.checked = true;
+    }
+}
+)
