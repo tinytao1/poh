8f98b91c978ae6fdf58d623f85eed5065867687c
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-02-27 13:46:00 -0800

    Update main.js
    Somethin' weird's goin' on w these.

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,32 @@ const cb2 = document.getElementById('cb2');
 const cb3 = document.getElementById('cb3');
 const cb4 = document.getElementById('cb4');
 
-cb2.addEventListener('click', function() {
+cb2.addEventListener('change', function() {
 	if (cb1.checked == true) {
 		cb1.checked = false;
     } else {cb1.checked = true;
+    }
+}
+)
+
+cb3.addEventListener('change', function() {
+	if (cb1.checked == true) {
+		cb1.checked = false;
+        cb2.checked = false;
+    } else {cb1.checked = true;
+    		cb2.checked = true;
+    }
+}
+)
+
+cb4.addEventListener('change', function() {
+	if (cb1.checked == true) {
+		cb1.checked = false;
+        cb2.checked = false;
+        cb3.checked = false;
+    } else {cb1.checked = true;
+    		cb2.checked = true;
+    		cb3.checked = true;
     }
 }
 )
