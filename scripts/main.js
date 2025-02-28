2ea48bfe2326ca3e90addeed856faa8041e69eb7
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-02-27 17:06:26 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,87 @@
-const cb1 = document.getElementById('cb1');
-const cb2 = document.getElementById('cb2');
-const cb3 = document.getElementById('cb3');
-const cb4 = document.getElementById('cb4');
 
-cb2.addEventListener('change', function() {
-	if (cb1.checked == true) {
-		cb1.checked = false;
-    } else {cb1.checked = true;
-    }
-}
-)
 
-cb3.addEventListener('change', function() {
-	if (cb1.checked == true) {
-		cb1.checked = false;
-        cb2.checked = false;
-    } else {cb1.checked = true;
-    		cb2.checked = true;
-    }
-}
-)
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+const c3 = document.getElementById('c3');
+const c4 = document.getElementById('c4');
+const c5 = document.getElementById('c5');
+const c6 = document.getElementById('c6');
 
-cb4.addEventListener('change', function() {
-	if (cb1.checked == true) {
-		cb1.checked = false;
-        cb2.checked = false;
-        cb3.checked = false;
-    } else {cb1.checked = true;
-    		cb2.checked = true;
-    		cb3.checked = true;
-    }
-}
-)
+c1.addEventListener('change', function () {
+	if (c1.checked == false || c1.checked == true) {
+		c2.checked = false;
+		c3.checked = false;
+		c4.checked = false;
+		c5.checked = false;
+		c6.checked = false;
+	}
+})
+
+c2.addEventListener('change', function () {
+	if (c2.checked == true) {
+		c1.checked = true;
+		c3.checked = false;
+		c4.checked = false;
+		c5.checked = false;
+		c6.checked = false;
+	} else {
+		c3.checked = false;
+		c4.checked = false;
+		c5.checked = false;
+		c6.checked = false;
+	}
+})
+
+c3.addEventListener('change', function () {
+	if (c3.checked == true) {
+		c1.checked = true;
+		c2.checked = true;
+		c4.checked = false;
+		c5.checked = false;
+		c6.checked = false;
+	} else {
+		c4.checked = false;
+		c5.checked = false;
+		c6.checked = false;
+	}
+})
+
+c4.addEventListener('change', function () {
+	if (c4.checked == true) {
+		c1.checked = true;
+		c2.checked = true;
+		c3.checked = true;
+		c5.checked = false;
+		c6.checked = false;
+	} else {
+		c5.checked = false;
+		c6.checked = false;
+	}
+})
+
+c5.addEventListener('change', function () {
+	if (c5.checked == true) {
+		c1.checked = true;
+		c2.checked = true;
+		c3.checked = true;
+		c4.checked = true;
+		c6.checked = false;
+	} else {
+		c6.checked = false;
+	}
+})
+
+c6.addEventListener('change', function () {
+	if (c6.checked == true) {
+		c1.checked = true;
+		c2.checked = true;
+		c3.checked = true;
+		c4.checked = true;
+		c5.checked = true;
+	}
+})
+
+
+
+
+
