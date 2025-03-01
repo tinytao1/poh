fa8c470ab01d3b529f995479a20832b2c4d36bb5
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 15:01:51 -0800

    m

--- scripts/chbxmain.js
+++ scripts/chbxmain.js
@@ -7,15 +7,22 @@ a1.addEventListener('change', function() {
 	if (a1.checked == true) {
 		acounter = 1;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
-
 	} else {
 		acounter = 0;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
-
 	}
 });
 
-document.getElementById('test').innerHTML = `acounter = ${acounter}`;
+a2.addEventListener('change', function() {
+	if (a2.checked == true) {
+		acounter = 2;
+		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
+	} else {
+		acounter = 0;
+		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
+	}
+});
+
 
 
 
