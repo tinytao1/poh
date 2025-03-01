d191c676454fea4f32f008c4d1bb232a5ebb364f
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 12:48:01 -0800

    Create variables.js

---
+++ scripts/variables.js
@@ -0,0 +1,103 @@
+// A-1
+const a1 = document.getElementById('a1');
+const a2 = document.getElementById('a2');
+const a3 = document.getElementById('a3');
+const a4 = document.getElementById('a4');
+const a5 = document.getElementById('a5');
+const a6 = document.getElementById('a6');
+
+// B-2
+const b1 = document.getElementById('b1');
+const b2 = document.getElementById('b2');
+const b3 = document.getElementById('b3');
+const b4 = document.getElementById('b4');
+const b5 = document.getElementById('b5');
+const b6 = document.getElementById('b6');
+
+// C-3
+/*
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+
+// D-4
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+
+// E-5
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+
+// F-6
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+
+// G-7
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+
+// H-8
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+
+// I-9
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
+*/
+// J-10
+const j1 = document.getElementById('j1');
+const j2 = document.getElementById('j2');
+const j3 = document.getElementById('j3');
+const j4 = document.getElementById('j4');
+const j5 = document.getElementById('j5');
+const j6 = document.getElementById('j6');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
