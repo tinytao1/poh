9fee14e89c0368876ee007eeb72b0a6daf2ec59d
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 13:42:00 -0800

    new

---
+++ scripts/chbxstoragebin.js
@@ -0,0 +1,85 @@
+
+
+//document.getElementById('test')
+//.innerHTML = "TEST";
+
+
+/*
+a1.addEventListener('change', function() {
+	if (a1.checked == false || a1.checked == true) {
+		a2.checked = false;
+		a3.checked = false;
+		a4.checked = false;
+		a5.checked = false;
+		a6.checked = false;
+	}
+});
+
+el2.addEventListener('change', function () {
+	if (el2.checked == true) {
+		el1.checked = true;
+		el3.checked = false;
+		el4.checked = false;
+		el5.checked = false;
+		el6.checked = false;
+	} else {
+		el3.checked = false;
+		el4.checked = false;
+		el5.checked = false;
+		el6.checked = false;
+	}
+});
+
+el3.addEventListener('change', function () {
+	if (el3.checked == true) {
+		el1.checked = true;
+		el2.checked = true;
+		el4.checked = false;
+		el5.checked = false;
+		el6.checked = false;
+	} else {
+		el4.checked = false;
+		el5.checked = false;
+		el6.checked = false;
+	}
+});
+
+el4.addEventListener('change', function () {
+	if (el4.checked == true) {
+		el1.checked = true;
+		el2.checked = true;
+		el3.checked = true;
+		el5.checked = false;
+		el6.checked = false;
+	} else {
+		el5.checked = false;
+		el6.checked = false;
+	}
+});
+
+el5.addEventListener('change', function () {
+	if (el5.checked == true) {
+		el1.checked = true;
+		el2.checked = true;
+		el3.checked = true;
+		el4.checked = true;
+		el6.checked = false;
+	} else {
+		el6.checked = false;
+	}
+});
+
+el6.addEventListener('change', function () {
+	if (el6.checked == true) {
+		el1.checked = true;
+		el2.checked = true;
+		el3.checked = true;
+		el4.checked = true;
+		el5.checked = true;
+	}
+});
+
+*/
+
+
+
