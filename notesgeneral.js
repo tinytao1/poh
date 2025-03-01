dd1c3de554c145613f8a36ad30e6724438acbd9f
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 12:37:31 -0800

    Create notesgeneral.js

---
+++ notesgeneral.js
@@ -0,0 +1,6 @@
+const 1 = document.getElementById('1');
+const 2 = document.getElementById('2');
+const 3 = document.getElementById('3');
+const 4 = document.getElementById('4');
+const 5 = document.getElementById('5');
+const 6 = document.getElementById('6');
