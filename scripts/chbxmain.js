fef5eff430dbab5f01c0105c240087d26e7d946f
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 14:56:05 -0800

    n

--- scripts/chbxmain.js
+++ scripts/chbxmain.js
@@ -6,6 +6,8 @@
 a1.addEventListener('change', function() {
 	if (a1.checked == true) {
 		acounter = 1;
+		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
+
 	} else {
 		acounter = 0;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
