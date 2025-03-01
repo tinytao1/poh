ef6f3de6034e858995e272a8787aeb3031f0af6e
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 13:58:26 -0800

    ok

--- notesgeneral.js
+++ notesgeneral.js
@@ -1,6 +1,31 @@
+
+
+/*
+VS Code supports multiple cursors 
+for fast simultaneous edits. You can add 
+secondary cursors (rendered thinner) with 
+
+WORKS:
+Alt+Click.
+ 
+Each cursor operates independently based 
+on the context it sits in. A common way to 
+add more cursors is with 
+
+FAULTY:
+Shift+Alt+Down or 
+Shift+Alt+Up 
+
+that insert cursors below or above.
+
+
+
 const 1 = document.getElementById('1');
 const 2 = document.getElementById('2');
 const 3 = document.getElementById('3');
 const 4 = document.getElementById('4');
 const 5 = document.getElementById('5');
 const 6 = document.getElementById('6');
+
+
+*/
--- scripts/chbxvariables.js
+++ scripts/chbxvariables.js
@@ -4,7 +4,7 @@
 
 
 
-/*
+
 // A-1
 const a1 = document.getElementById('a1');
 const a2 = document.getElementById('a2');
@@ -22,69 +22,69 @@ const b5 = document.getElementById('b5');
 const b6 = document.getElementById('b6');
 
 // C-3
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+const c3 = document.getElementById('c3');
+const c4 = document.getElementById('c4');
+const c5 = document.getElementById('c5');
+const c6 = document.getElementById('c6');
 
 // D-4
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
+const d1 = document.getElementById('d1');
+const d2 = document.getElementById('d2');
+const d3 = document.getElementById('d3');
+const d4 = document.getElementById('d4');
+const d5 = document.getElementById('d5');
+const d6 = document.getElementById('d6');
 
 // E-5
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
+const e1 = document.getElementById('e1');
+const e2 = document.getElementById('e2');
+const e3 = document.getElementById('e3');
+const e4 = document.getElementById('e4');
+const e5 = document.getElementById('e5');
+const e6 = document.getElementById('e6');
 
 // F-6
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
+const f1 = document.getElementById('f1');
+const f2 = document.getElementById('f2');
+const f3 = document.getElementById('f3');
+const f4 = document.getElementById('f4');
+const f5 = document.getElementById('f5');
+const f6 = document.getElementById('f6');
 
 // G-7
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
+const g1 = document.getElementById('g1');
+const g2 = document.getElementById('g2');
+const g3 = document.getElementById('g3');
+const g4 = document.getElementById('g4');
+const g5 = document.getElementById('g5');
+const g6 = document.getElementById('g6');
 
 // H-8
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
+const h1 = document.getElementById('h1');
+const h2 = document.getElementById('h2');
+const h3 = document.getElementById('h3');
+const h4 = document.getElementById('h4');
+const h5 = document.getElementById('h5');
+const h6 = document.getElementById('h6');
 
 // I-9
-const 1 = document.getElementById('1');
-const 2 = document.getElementById('2');
-const 3 = document.getElementById('3');
-const 4 = document.getElementById('4');
-const 5 = document.getElementById('5');
-const 6 = document.getElementById('6');
-
+const i1 = document.getElementById('i1');
+const i2 = document.getElementById('i2');
+const i3 = document.getElementById('i3');
+const i4 = document.getElementById('i4');
+const i5 = document.getElementById('i5');
+const i6 = document.getElementById('i6');
+h
 // J-10
-const j1 = document.getElementById('j1');
+const j1 = document.hgetElementById('j1');
 const j2 = document.getElementById('j2');
 const j3 = document.getElementById('j3');
 const j4 = document.getElementById('j4');
 const j5 = document.getElementById('j5');
 const j6 = document.getElementById('j6');
-*/
+
 
 
 
