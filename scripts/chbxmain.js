7ad39e50cc985c19a9609e3123567837ba5461f1
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-01 16:08:35 -0800

    Update chbxmain.js

--- scripts/chbxmain.js
+++ scripts/chbxmain.js
@@ -1,29 +1,14 @@
 
 
+
+
+
+
 let CDS = '3/1/2025';
-let CTS = '3:58 PM';
+let CTS = '4:08 PM';
 
 document.getElementById('committed')
 .innerHTML = `committed: ${CDS}<br />${CTS}.`;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 a1.addEventListener('change', function() {
 	if (a1.checked == true) {
@@ -65,7 +50,6 @@ a2.addEventListener('change', function() {
 	}
 });
 
-
 a3.addEventListener('change', function() {
 	if (a3.checked == true) {
 		acounter = 3;
@@ -86,7 +70,6 @@ a3.addEventListener('change', function() {
 	}
 });
 
-
 a4.addEventListener('change', function() {
 	if (a4.checked == true) {
 		acounter = 4;
@@ -99,53 +82,50 @@ a4.addEventListener('change', function() {
 	} else {
 		acounter = 0;
 		a1.checked = false;
-		a3.checked = false;
 		a2.checked = false;
+		a3.checked = false;
 		a5.checked = false;
 		a6.checked = false;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
 	}
 });
-
 
 a5.addEventListener('change', function() {
 	if (a5.checked == true) {
 		acounter = 5;
 		a1.checked = true;
+		a2.checked = true;
 		a3.checked = true;
 		a4.checked = true;
-		a5.checked = true;
 		a6.checked = false;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
 	} else {
 		acounter = 0;
 		a1.checked = false;
+		a2.checked = false;
 		a3.checked = false;
 		a4.checked = false;
-		a5.checked = false;
-		a2.checked = false;
+		a6.checked = false;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
 	}
 });
-
-
 
 a6.addEventListener('change', function() {
 	if (a6.checked == true) {
 		acounter = 6;
 		a1.checked = true;
+		a2.checked = true;
 		a3.checked = true;
 		a4.checked = true;
 		a5.checked = true;
-		a6.checked = true;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
 	} else {
 		acounter = 0;
 		a1.checked = false;
+		a2.checked = false;
 		a3.checked = false;
 		a4.checked = false;
 		a5.checked = false;
-		a2.checked = false;
 		document.getElementById('test').innerHTML = `acounter = ${acounter}`;
 	}
 });
