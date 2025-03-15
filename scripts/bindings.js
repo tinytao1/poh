144c6750d52d511ee2ad446aaa5e66a27d42a9a1
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-15 12:02:29 -0700

    Update and rename chbxvariables.js to bindings.js

--- scripts/bindings.js
+++ scripts/bindings.js
@@ -15,83 +15,74 @@ let hcounter = 0;
 let icounter = 0;
 let jcounter = 0;
 
-// Buttons Checkbox view
+// Buttons UNCHECK All, top & bottom
 const uncheckalltop = document.getElementById('uncheckalltop');
 const uncheckallbtm = document.getElementById('uncheckallbtm');
 
-// A-1 thru J-10
+// A-1 thru J-10 checkbox rows A- J 
 const a1 = document.getElementById('a1');
 const a2 = document.getElementById('a2');
 const a3 = document.getElementById('a3');
 const a4 = document.getElementById('a4');
 const a5 = document.getElementById('a5');
 const a6 = document.getElementById('a6');
-
-// B-2
+// B-2 checkbox row B
 const b1 = document.getElementById('b1');
 const b2 = document.getElementById('b2');
 const b3 = document.getElementById('b3');
 const b4 = document.getElementById('b4');
 const b5 = document.getElementById('b5');
 const b6 = document.getElementById('b6');
-
-// C-3
+// C-3 checkbox row C
 const c1 = document.getElementById('c1');
 const c2 = document.getElementById('c2');
 const c3 = document.getElementById('c3');
 const c4 = document.getElementById('c4');
 const c5 = document.getElementById('c5');
 const c6 = document.getElementById('c6');
-
-// D-4
+// D-4 checkbox row 
 const d1 = document.getElementById('d1');
 const d2 = document.getElementById('d2');
 const d3 = document.getElementById('d3');
 const d4 = document.getElementById('d4');
 const d5 = document.getElementById('d5');
 const d6 = document.getElementById('d6');
-
-// E-5
+// E-5 checkbox row E
 const e1 = document.getElementById('e1');
 const e2 = document.getElementById('e2');
 const e3 = document.getElementById('e3');
 const e4 = document.getElementById('e4');
 const e5 = document.getElementById('e5');
 const e6 = document.getElementById('e6');
-
-// F-6
+// F-6 checkbox row F
 const f1 = document.getElementById('f1');
 const f2 = document.getElementById('f2');
 const f3 = document.getElementById('f3');
 const f4 = document.getElementById('f4');
 const f5 = document.getElementById('f5');
 const f6 = document.getElementById('f6');
-
-// G-7
+// G-7 checkbox row G
 const g1 = document.getElementById('g1');
 const g2 = document.getElementById('g2');
 const g3 = document.getElementById('g3');
 const g4 = document.getElementById('g4');
 const g5 = document.getElementById('g5');
 const g6 = document.getElementById('g6');
-
-// H-8
+// H-8 checkbox row H
 const h1 = document.getElementById('h1');
 const h2 = document.getElementById('h2');
 const h3 = document.getElementById('h3');
 const h4 = document.getElementById('h4');
 const h5 = document.getElementById('h5');
 const h6 = document.getElementById('h6');
-
-// I-9
+// I-9 checkbox row I
 const i1 = document.getElementById('i1');
 const i2 = document.getElementById('i2');
 const i3 = document.getElementById('i3');
 const i4 = document.getElementById('i4');
 const i5 = document.getElementById('i5');
 const i6 = document.getElementById('i6');
-
-// J-10
+// J-10 checkbox row J
 const j1 = document.getElementById('j1');
 const j2 = document.getElementById('j2');
 const j3 = document.getElementById('j3');
