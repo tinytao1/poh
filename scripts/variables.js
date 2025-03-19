40294bae27885db0209cfb9503f5e375524b4762
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-18 18:22:40 -0700

    Update variables.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -89,6 +89,16 @@ const j6 = document.getElementById('j6');
 
 const allcheckboxesarray = [a1, a2, a3, a4, a5, a6, b1, b2, b3, b4, b5, b6, c1, c2, c3, c4, c5, c6, d1, d2, d3, d4, d5, d6, e1, e2, e3, e4, e5, e6, f1, f2, f3, f4, f5, f6, g1, g2, g3, g4, g5, g6, h1, h2, h3, h4, h5, h6, i1, i2, i3, i4, i5, i6, j1, j2, j3, j4, j5, j6];
 
+const checkboxrowaarray = [a1, a2, a3, a4, a5, a6];
+const checkboxrowbarray = [b1, b2, b3, b4, b5, b6];
+const checkboxrowcarray = [c1, c2, c3, c4, c5, c6];
+const checkboxrowdarray = [d1, d2, d3, d4, d5, d6];
+const checkboxrowearray = [e1, e2, e3, e4, e5, e6];
+const checkboxrowfarray = [f1, f2, f3, f4, f5, f6];
+const checkboxrowgarray = [g1, g2, g3, g4, g5, g6];
+const checkboxrowharray = [h1, h2, h3, h4, h5, h6];
+const checkboxrowiarray = [i1, i2, i3, i4, i5, i6];
+const checkboxrowjarray = [j1, j2, j3, j4, j5, j6];
 
 
 
