7609fe13cff54a153fa6d9c59184a20fee630a02
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-20 12:48:57 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@
 
 
 //   /////////////////// variable 'acounter' appears is being automatically declared for use by the browser ////////
-let CDS = 'Wednesday, 19 March, 2025';
-let CTS = '11:30 AM';
+let CDS = 'Thursday, 20 March, 2025';
+let CTS = '12:48 PM';
 
 document.getElementById('committed')
 .innerHTML = `committed: ${CDS}<br />${CTS}.`;
@@ -12,125 +12,7 @@ document.getElementById('committed')
 uncheckalltop.addEventListener('click', uncheckallloop);
 uncheckallbtm.addEventListener('click', uncheckallloop);
 
-a1.addEventListener('change', function() {
-	if (a1.checked == true) {
-		acounter = 1;
-		a2.checked = false;
-		a3.checked = false;
-		a4.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	} else {
-		acounter = 0;
-		a2.checked = false;
-		a3.checked = false;
-		a4.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	}
-});
 
-a2.addEventListener('change', function() {
-	if (a2.checked == true) {
-		acounter = 2;
-		a1.checked = true;
-		a3.checked = false;
-		a4.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	} else {
-		acounter = 0;
-		a1.checked = false;
-		a3.checked = false;
-		a4.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	}
-});
-
-a3.addEventListener('change', function() {
-	if (a3.checked == true) {
-		acounter = 3;
-		a1.checked = true;
-		a2.checked = true;
-		a4.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	} else {
-		acounter = 0;
-		a1.checked = false;
-		a2.checked = false;
-		a4.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	}
-});
-
-a4.addEventListener('change', function() {
-	if (a4.checked == true) {
-		acounter = 4;
-		a1.checked = true;
-		a2.checked = true;
-		a3.checked = true;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	} else {
-		acounter = 0;
-		a1.checked = false;
-		a2.checked = false;
-		a3.checked = false;
-		a5.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	}
-});
-
-a5.addEventListener('change', function() {
-	if (a5.checked == true) {
-		acounter = 5;
-		a1.checked = true;
-		a2.checked = true;
-		a3.checked = true;
-		a4.checked = true;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	} else {
-		acounter = 0;
-		a1.checked = false;
-		a2.checked = false;
-		a3.checked = false;
-		a4.checked = false;
-		a6.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	}
-});
-
-a6.addEventListener('change', function() {
-	if (a6.checked == true) {
-		acounter = 6;
-		a1.checked = true;
-		a2.checked = true;
-		a3.checked = true;
-		a4.checked = true;
-		a5.checked = true;
-		counterrowael.innerHTML = `${acounter}`;
-	} else {
-		acounter = 0;
-		a1.checked = false;
-		a2.checked = false;
-		a3.checked = false;
-		a4.checked = false;
-		a5.checked = false;
-		counterrowael.innerHTML = `${acounter}`;
-	}
-});
 
 
 
