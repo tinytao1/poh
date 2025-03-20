7852dd29c74de3646efe88f461abf866acbe4eba
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-20 12:46:14 -0700

    Create checkboxeventlisteners.js

---
+++ scripts/checkboxeventlisteners.js
@@ -0,0 +1,180 @@
+a1.addEventListener('change', function() {
+	checkbox1of6(...rowaarray);
+} )
+a2.addEventListener('change', function() {
+	checkbox2of6(...rowaarray);
+} )
+a3.addEventListener('change', function() {
+	checkbox3of6(...rowaarray);
+})
+a4.addEventListener('change', function() {
+	checkbox4of6(...rowaarray);
+} )
+a5.addEventListener('change', function() {
+	checkbox5of6(...rowaarray);
+} )
+a6.addEventListener('change', function() {
+	checkbox6of6(...rowaarray);
+} )
+b1.addEventListener('change', function() {
+	checkbox1of6(...rowbarray);
+} )
+b2.addEventListener('change', function() {
+	checkbox2of6(...rowbarray);
+} )
+b3.addEventListener('change', function() {
+	checkbox3of6(...rowbarray);
+})
+b4.addEventListener('change', function() {
+	checkbox4of6(...rowbarray);
+} )
+b5.addEventListener('change', function() {
+	checkbox5of6(...rowbarray);
+} )
+b6.addEventListener('change', function() {
+	checkbox6of6(...rowbarray);
+} )
+c1.addEventListener('change', function() {
+	checkbox1of6(...rowcarray);
+} )
+c2.addEventListener('change', function() {
+	checkbox2of6(...rowcarray);
+} )
+c3.addEventListener('change', function() {
+	checkbox3of6(...rowcarray);
+})
+c4.addEventListener('change', function() {
+	checkbox4of6(...rowcarray);
+} )
+c5.addEventListener('change', function() {
+	checkbox5of6(...rowcarray);
+} )
+c6.addEventListener('change', function() {
+	checkbox6of6(...rowcarray);
+} )
+d1.addEventListener('change', function() {
+	checkbox1of6(...rowdarray);
+} )
+d2.addEventListener('change', function() {
+	checkbox2of6(...rowdarray);
+} )
+d3.addEventListener('change', function() {
+	checkbox3of6(...rowdarray);
+})
+d4.addEventListener('change', function() {
+	checkbox4of6(...rowdarray);
+} )
+d5.addEventListener('change', function() {
+	checkbox5of6(...rowdarray);
+} )
+d6.addEventListener('change', function() {
+	checkbox6of6(...rowdarray);
+} )
+e1.addEventListener('change', function() {
+	checkbox1of6(...rowearray);
+} )
+e2.addEventListener('change', function() {
+	checkbox2of6(...rowearray);
+} )
+e3.addEventListener('change', function() {
+	checkbox3of6(...rowearray);
+})
+e4.addEventListener('change', function() {
+	checkbox4of6(...rowearray);
+} )
+e5.addEventListener('change', function() {
+	checkbox5of6(...rowearray);
+} )
+e6.addEventListener('change', function() {
+	checkbox6of6(...rowearray);
+} )
+f1.addEventListener('change', function() {
+	checkbox1of6(...rowfarray);
+} )
+f2.addEventListener('change', function() {
+	checkbox2of6(...rowfarray);
+} )
+f3.addEventListener('change', function() {
+	checkbox3of6(...rowfarray);
+})
+f4.addEventListener('change', function() {
+	checkbox4of6(...rowfarray);
+} )
+f5.addEventListener('change', function() {
+	checkbox5of6(...rowfarray);
+} )
+f6.addEventListener('change', function() {
+	checkbox6of6(...rowfarray);
+} )
+g1.addEventListener('change', function() {
+	checkbox1of6(...rowgarray);
+} )
+g2.addEventListener('change', function() {
+	checkbox2of6(...rowgarray);
+} )
+g3.addEventListener('change', function() {
+	checkbox3of6(...rowgarray);
+})
+g4.addEventListener('change', function() {
+	checkbox4of6(...rowgarray);
+} )
+g5.addEventListener('change', function() {
+	checkbox5of6(...rowgarray);
+} )
+g6.addEventListener('change', function() {
+	checkbox6of6(...rowgarray);
+} )
+h1.addEventListener('change', function() {
+	checkbox1of6(...rowharray);
+} )
+h2.addEventListener('change', function() {
+	checkbox2of6(...rowharray);
+} )
+h3.addEventListener('change', function() {
+	checkbox3of6(...rowharray);
+})
+h4.addEventListener('change', function() {
+	checkbox4of6(...rowharray);
+} )
+h5.addEventListener('change', function() {
+	checkbox5of6(...rowharray);
+} )
+h6.addEventListener('change', function() {
+	checkbox6of6(...rowharray);
+} )
+i1.addEventListener('change', function() {
+	checkbox1of6(...rowiarray);
+} )
+i2.addEventListener('change', function() {
+	checkbox2of6(...rowiarray);
+} )
+i3.addEventListener('change', function() {
+	checkbox3of6(...rowiarray);
+})
+i4.addEventListener('change', function() {
+	checkbox4of6(...rowiarray);
+} )
+i5.addEventListener('change', function() {
+	checkbox5of6(...rowiarray);
+} )
+i6.addEventListener('change', function() {
+	checkbox6of6(...rowiarray);
+} )
+j1.addEventListener('change', function() {
+	checkbox1of6(...rowjarray);
+} )
+j2.addEventListener('change', function() {
+	checkbox2of6(...rowjarray);
+} )
+j3.addEventListener('change', function() {
+	checkbox3of6(...rowjarray);
+})
+j4.addEventListener('change', function() {
+	checkbox4of6(...rowjarray);
+} )
+j5.addEventListener('change', function() {
+	checkbox5of6(...rowjarray);
+} )
+j6.addEventListener('change', function() {
+	checkbox6of6(...rowjarray);
+} )
