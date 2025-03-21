080434d694459fbec55c7d0df7de7cee2a7337c7
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-20 23:21:23 -0700

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -2,10 +2,8 @@
 
 
 function addrows() {
-      counterproducetotal= counterrowa+counterrowb+counterrowc
-						  +counterrowd+counterrowe+counterrowf+counterrowg
-								+counterrowh+counterrowi+counterrowj;
-							return counterproducetotal;
+       //counterproducetotal= counterrowa+counterrowb+counterrowc+counterrowd+counterrowe+counterrowf+counterrowg+counterrowh+counterrowi+counterrowj;
+							return 5; //counterproducetotal;
 }
 function checkbox1of6(id1,id2,id3,id4,id5,id6,counterrow,counterrowel) {
 	if (id1.checked == true) {
