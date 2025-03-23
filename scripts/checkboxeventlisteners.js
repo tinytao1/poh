0e7fe8ff727c522236a8cb0047bb9a73f65b5b78
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-03-23 15:29:52 -0700

    Update checkboxeventlisteners.js

--- scripts/checkboxeventlisteners.js
+++ scripts/checkboxeventlisteners.js
@@ -43,151 +43,201 @@ b4.addEventListener('change', function() {
 } )
 b5.addEventListener('change', function() {
 	checkbox5of6(...rowbarray);
+	calculateproducetotal();
 } )
 b6.addEventListener('change', function() {
 	checkbox6of6(...rowbarray);
+	calculateproducetotal();
 } )
 c1.addEventListener('change', function() {
 	checkbox1of6(...rowcarray);
+	calculateproducetotal();
 } )
 c2.addEventListener('change', function() {
 	checkbox2of6(...rowcarray);
+	calculateproducetotal();
 } )
 c3.addEventListener('change', function() {
 	checkbox3of6(...rowcarray);
+	calculateproducetotal();
 })
 c4.addEventListener('change', function() {
 	checkbox4of6(...rowcarray);
+	calculateproducetotal();
 } )
 c5.addEventListener('change', function() {
 	checkbox5of6(...rowcarray);
+	calculateproducetotal();
 } )
 c6.addEventListener('change', function() {
 	checkbox6of6(...rowcarray);
+	calculateproducetotal();
 } )
 d1.addEventListener('change', function() {
 	checkbox1of6(...rowdarray);
+	calculateproducetotal();
 } )
 d2.addEventListener('change', function() {
 	checkbox2of6(...rowdarray);
+	calculateproducetotal();
 } )
 d3.addEventListener('change', function() {
 	checkbox3of6(...rowdarray);
+	calculateproducetotal();
 })
 d4.addEventListener('change', function() {
 	checkbox4of6(...rowdarray);
+	calculateproducetotal();
 } )
 d5.addEventListener('change', function() {
 	checkbox5of6(...rowdarray);
+	calculateproducetotal();
 } )
 d6.addEventListener('change', function() {
 	checkbox6of6(...rowdarray);
+	calculateproducetotal();
 } )
 e1.addEventListener('change', function() {
 	checkbox1of6(...rowearray);
+	calculateproducetotal();
 } )
 e2.addEventListener('change', function() {
 	checkbox2of6(...rowearray);
+	calculateproducetotal();
 } )
 e3.addEventListener('change', function() {
 	checkbox3of6(...rowearray);
+	calculateproducetotal();
 })
 e4.addEventListener('change', function() {
 	checkbox4of6(...rowearray);
+	calculateproducetotal();
 } )
 e5.addEventListener('change', function() {
 	checkbox5of6(...rowearray);
+	calculateproducetotal();
 } )
 e6.addEventListener('change', function() {
 	checkbox6of6(...rowearray);
+	calculateproducetotal();
 } )
 f1.addEventListener('change', function() {
 	checkbox1of6(...rowfarray);
+	calculateproducetotal();
 } )
 f2.addEventListener('change', function() {
 	checkbox2of6(...rowfarray);
+	calculateproducetotal();
 } )
 f3.addEventListener('change', function() {
 	checkbox3of6(...rowfarray);
+	calculateproducetotal();
 })
 f4.addEventListener('change', function() {
 	checkbox4of6(...rowfarray);
+	calculateproducetotal();
 } )
 f5.addEventListener('change', function() {
 	checkbox5of6(...rowfarray);
+	calculateproducetotal();
 } )
 f6.addEventListener('change', function() {
 	checkbox6of6(...rowfarray);
+	calculateproducetotal();
 } )
 g1.addEventListener('change', function() {
 	checkbox1of6(...rowgarray);
+	calculateproducetotal();
 } )
 g2.addEventListener('change', function() {
 	checkbox2of6(...rowgarray);
+	calculateproducetotal();
 } )
 g3.addEventListener('change', function() {
 	checkbox3of6(...rowgarray);
+	calculateproducetotal();
 })
 g4.addEventListener('change', function() {
 	checkbox4of6(...rowgarray);
+	calculateproducetotal();
 } )
 g5.addEventListener('change', function() {
 	checkbox5of6(...rowgarray);
+	calculateproducetotal();
 } )
 g6.addEventListener('change', function() {
 	checkbox6of6(...rowgarray);
+	calculateproducetotal();
 } )
 h1.addEventListener('change', function() {
 	checkbox1of6(...rowharray);
+	calculateproducetotal();
 } )
 h2.addEventListener('change', function() {
 	checkbox2of6(...rowharray);
+	calculateproducetotal();
 } )
 h3.addEventListener('change', function() {
 	checkbox3of6(...rowharray);
+	calculateproducetotal();   
 })
 h4.addEventListener('change', function() {
 	checkbox4of6(...rowharray);
+	calculateproducetotal();   
 } )
 h5.addEventListener('change', function() {
 	checkbox5of6(...rowharray);
+	calculateproducetotal();   
 } )
 h6.addEventListener('change', function() {
 	checkbox6of6(...rowharray);
+	calculateproducetotal();   
 } )
 i1.addEventListener('change', function() {
 	checkbox1of6(...rowiarray);
+	calculateproducetotal();   
 } )
 i2.addEventListener('change', function() {
 	checkbox2of6(...rowiarray);
+	calculateproducetotal();   
 } )
 i3.addEventListener('change', function() {
 	checkbox3of6(...rowiarray);
+	calculateproducetotal();   
 })
 i4.addEventListener('change', function() {
 	checkbox4of6(...rowiarray);
+	calculateproducetotal();   
 } )
 i5.addEventListener('change', function() {
 	checkbox5of6(...rowiarray);
+	calculateproducetotal();   
 } )
 i6.addEventListener('change', function() {
 	checkbox6of6(...rowiarray);
+	calculateproducetotal();   
 } )
 j1.addEventListener('change', function() {
 	checkbox1of6(...rowjarray);
+	calculateproducetotal();   
 } )
 j2.addEventListener('change', function() {
 	checkbox2of6(...rowjarray);
+	calculateproducetotal();   
 } )
 j3.addEventListener('change', function() {
 	checkbox3of6(...rowjarray);
+	calculateproducetotal();   
 })
 j4.addEventListener('change', function() {
 	checkbox4of6(...rowjarray);
+	calculateproducetotal();   
 } )
 j5.addEventListener('change', function() {
 	checkbox5of6(...rowjarray);
+	calculateproducetotal();   
 } )
 j6.addEventListener('change', function() {
 	checkbox6of6(...rowjarray);
+	calculateproducetotal();   
 } )
