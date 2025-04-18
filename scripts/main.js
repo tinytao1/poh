d2fbb24ce4c70c0ce7bdf0f2ccd7cca597f61099
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-04-18 15:37:17 -0700

    added "getrow[letter]total() to 60 event listeners.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 //   /////////////////// variable 'acounter' appears is being automatically declared for use by the browser ////////
 let CDS = 'April 18 2025';
-let CTS = 'Friday 3:23 PM';
+let CTS = 'Friday 3:36 PM';
 
 document.getElementById('committed')
 .innerHTML = `version: ${CDS}<br />${CTS}.`;
