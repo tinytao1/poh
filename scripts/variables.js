2cc6c2a0e58fc3faea6ab79177f44fcdcb7db228
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-04-20 15:58:09 -0700

    Update variables.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -141,6 +141,7 @@ const rowiarray = [i1, i2, i3, i4, i5, i6, counterrowi, counterrowiel];
 const rowjarray = [j1, j2, j3, j4, j5, j6, counterrowj, counterrowjel];
 const allcheckboxesarray = [a1, a2, a3, a4, a5, a6, b1, b2, b3, b4, b5, b6, c1, c2, c3, c4, c5, c6, d1, d2, d3, d4, d5, d6, e1, e2, e3, e4, e5, e6, f1, f2, f3, f4, f5, f6, g1, g2, g3, g4, g5, g6, h1, h2, h3, h4, h5, h6, i1, i2, i3, i4, i5, i6, j1, j2, j3, j4, j5, j6];
 
+let viewdivtitleel = document.getElementById('viewdivtitleel');
 
 
 
