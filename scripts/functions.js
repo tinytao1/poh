c672a5e03f40734384cf2b7b98af4d67f25e31eb
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-04-21 01:42:05 -0700

    Update functions.js

--- scripts/functions.js
+++ scripts/functions.js
@@ -220,8 +220,7 @@ function uncheckallloop() {
 			getrowitotal();
 			getrowjtotal();
 			viewdivtitleel.style.display = 'block';
-			itemadisplaynameel.style.display = "none";
-			itemadisplaynumberel.style.display = "none";
+			itemadisplayrowel.style.display = "none";
 			// calculateproducetotal();
 		}}}
 function calculateproducetotal() {
