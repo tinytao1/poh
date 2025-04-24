cb222fede779a778eace672f37690b01edd81f29
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-04-24 15:25:50 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let CTS = 'Thursday 3:22 PM';
-let CDS = 'April 24 2025';
+let CDS = '4/24/2025'; /* April 24 2025 */
 
 document.getElementById('committed')
 .innerHTML = `version: ${CTS}<br />${CDS}.`;
