a2cd7979460f8a8fd74db6cabc38b9d2248c28c4
tinytao1 <86927506+tinytao1@users.noreply.github.com>
2025-04-28 13:28:50 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ let CTS = '4/28/2025, 1:30 PM, Monday';
 let CDS = ''; /* April 24 2025 */
 
 document.getElementById('committed')
-.innerHTML = `version: ${CTS}<br />${CDS}.`;
+.innerHTML = `version: ${CTS}.`  // <br />${CDS}.`;
 
 uncheckalltop.addEventListener('click', uncheckallloop);
 uncheckallbtm.addEventListener('click', uncheckallloop);
